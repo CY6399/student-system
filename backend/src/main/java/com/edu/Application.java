cc25b111aafd4636267e2294c44d234c54dae861
billiondots <3414963359@qq.com>
2026-05-25 08:53:06 +0800

    完成任务10 Maven初始化

---
+++ backend/src/main/java/com/edu/Application.java
@@ -0,0 +1,15 @@
+package com.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 教学数字平台管理系统 —— 启动类
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
